5be6ec1012dca5dbcdff1aa7e95598223fee4236
Gahee Heo <ghbooth12@gmail.com>
2016-03-31 23:36:31 -0500

    ch1-continued-add-username: add role: completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ActiveRecord::Base
+  after_initialize :init
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  validates :username, length: { minimum: 3, maximum: 30 }, presence: true
+
+  enum role: [:standard, :premium, :admin]
+
+  def init
+    self.role ||= :standard if self.has_attribute? :role
+  end
 end
