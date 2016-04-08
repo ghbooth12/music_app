8c4adfb2869d2a000507a853201e8c6de6988d30
Gahee Heo <ghbooth12@gmail.com>
2016-04-07 22:09:03 -0500

    ch9-user-roles: Showed all tags, Added select option value, Set up Stripe

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,7 @@
 class Genre < ActiveRecord::Base
   has_many :profiles, dependent: :destroy
 
+  before_save { self.name = name.capitalize }
+
   validates :name, length: { in: 2..30 }, presence: true
 end
