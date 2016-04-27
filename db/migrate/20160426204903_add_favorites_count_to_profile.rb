f2e55c66603201f9533e7e228c2e1260636b9cc5
Gahee Heo <ghbooth12@gmail.com>
2016-04-27 09:39:32 -0500

    ch23-counter-cache-review: completed

--- db/migrate/20160426204903_add_favorites_count_to_profile.rb
+++ db/migrate/20160426204903_add_favorites_count_to_profile.rb
@@ -1,5 +1,11 @@
 class AddFavoritesCountToProfile < ActiveRecord::Migration
   def change
     add_column :profiles, :favorites_count, :integer, default: 0
+
+    # Profile.reset_column_information
+    # Profile.all.each do |profile|
+    #   profile.favorites_count = profile.favorites.count
+    #   profile.save!
+    # end
   end
 end
